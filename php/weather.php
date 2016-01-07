0fd842ae488eed6d866f6c70278dd265eeef0766
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-01-06 21:30:53 -0800

    Completed IP location based weather challenge

--- php/weather.php
+++ php/weather.php
@@ -6,8 +6,13 @@
 </head>
 <body>
     <?php
-        $response = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Seattle,us&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0'));
-        echo $response;
+        $locationData = json_decode(file_get_contents('http://ip-api.com/json'));     
+        
+        $weatherapiQuery = 'http://api.openweathermap.org/data/2.5/weather?q='.$locationData->city.",".$locationData->countryCode.'&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0'; 
+        
+        $weatherData = json_decode(file_get_contents($weatherapiQuery));
+         
+        echo ("The weather in {$locationData->city}, $locationData->countryCode is {$weatherData->weather[0]->description} with a current temperature of  {$weatherData->main->temp}.");
     ?>
 </body>
 </html>
