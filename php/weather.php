bdc79c8dcb1e8e91aaa2f98e676445e7a795aa06
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-01-06 21:54:41 -0800

    Completed Weather challenge using both URL Query as well as IP location based lookup

--- php/weather.php
+++ php/weather.php
@@ -6,13 +6,25 @@
 </head>
 <body>
     <?php
-        $locationData = json_decode(file_get_contents('http://ip-api.com/json'));     
+    
+        $url = parse_url($_SERVER['REQUEST_URI']);
+
+        $location = substr($url['path'], 12);
+        if (strlen($location) == 0) {
+            $locationData = json_decode(file_get_contents('http://ip-api.com/json'));
+            $city = $locationData->city;
+            $countryCode = $locationData->countryCode;
+        } else {
+            $locationData = explode(",", $location);
+            $city = substr($locationData[0], 1);
+            $countryCode = substr($locationData[1], 0, 2);
+        }
         
-        $weatherapiQuery = 'http://api.openweathermap.org/data/2.5/weather?q='.$locationData->city.",".$locationData->countryCode.'&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0'; 
+        $weatherapiQuery = 'http://api.openweathermap.org/data/2.5/weather?q='.$city.",".$countryCode.'&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0'; 
         
         $weatherData = json_decode(file_get_contents($weatherapiQuery));
          
-        echo ("The weather in {$locationData->city}, $locationData->countryCode is {$weatherData->weather[0]->description} with a current temperature of  {$weatherData->main->temp}.");
+        echo ("The weather in $city, $countryCode is {$weatherData->weather[0]->description} with a current temperature of  {$weatherData->main->temp}.");
     ?>
 </body>
 </html>
