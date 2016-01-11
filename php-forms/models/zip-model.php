38be11b8823014f9349a1c8c5390839d4608ae65
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-01-11 10:30:26 -0800

    end of class commit: successfully can connect to local database and search weather api for results

---
+++ php-forms/models/zip-model.php
@@ -0,0 +1,20 @@
+<?php
+class Zips{
+    protected $conn;
+    
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+    
+    public function search($q) {
+        $sql = 'select * from zips where zip=? or primary_city=?';
+        $stmt = $this->conn->prepare($sql);
+        $success = $stmt->execute(array($q,$q));
+        if (!$success) {
+            trigger_error($stmt->errorInfo());
+        } else {
+            return $stmt->fetchAll();
+        }
+    }
+}
+?>
